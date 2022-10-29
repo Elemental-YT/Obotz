8fc105df2a8669dc1e26c2c32493245b9028f424
Rohan Sodavaram <rohan.sodavaram@gmail.com>
2022-10-29 12:39:07 -0400

    Update EXP26_LAB.ino

--- EXP26_LAB/EXP26_LAB.ino
+++ EXP26_LAB/EXP26_LAB.ino
@@ -5,50 +5,46 @@
 #include <LCD.h>
 int main(void)
 {
-  LCD lcd;
-  lcd.init();
-  int LED;
-  int BUZZ;
-  int TIMER;
-  TCCR0 = 0x03;
-  DDRD = 0xFB;
-  DDRB = 0xFF;
-  Serial.begin(2000000);
-  while(1)
+  LCD lcd;                                    // Creating an object of class LCD
+  lcd.init();                                 // Initializig the LCD
+  int LED;                                    // Variable for LED 
+  int BUZZ;                                   // Variable for Buzzer
+  DDRD = 0xFB;                                // Output port D
+  Serial.begin(2000000);                      // Initialize serial communication at baud rate of 2000000 bps 
+  while(1)                                    // Start the Forever Loop
   {
-    ADMUX = 0x40;
-    ADCSRA = 0xC7;
-    while(checkbit(ADCSRA, bitn(ADSC)));
-    Serial.println(ADCW);
-    LED = ADCW;
-    _delay_ms(500);
-    ADMUX = 0x41;
-    ADCSRA = 0xC7;
-    while(checkbit(ADCSRA, bitn(ADSC)));
-    Serial.println(ADCW);
-    BUZZ = ADCW;
+    ADMUX = 0x40;                             // Set A0 As the ADC conversion pin
+    ADCSRA = 0xC7;                            // Start the ADC conversion
+    while(checkbit(ADCSRA, bitn(ADSC)));      // Wait until the ADC conversion is done
+    Serial.println(ADCW);                     // Print the output onto the Serial Moniter
+    LED = ADCW;                               // Save the output into the Variable LED
+    _delay_ms(500);                           // Wait for 500 miliseconds
+    ADMUX = 0x41;                             // Set A1 As the ADC conversion pin
+    ADCSRA = 0xC7;                            // Start the ADC conversion
+    while(checkbit(ADCSRA, bitn(ADSC)));      // Wait until the ADC conversion is done
+    Serial.println(ADCW);                     // Print the output onto the Serial Moniter
+    BUZZ = ADCW;                              // Save the output into the Variable BUZZ
     
-    if(LED > 350)
+    if(LED > 350)                             // If LED is greater than 350
     {
       
-      lcd.string("LED");
-      PORTD = 0b00100000;
+      lcd.string("Alert! ");                  // Print on the LCD Alert!
+      PORTD = 0b00100000;                     // Set PD5 as High and everything else Low
     }
     
-    if(BUZZ >  350)
+    if(BUZZ >  350)                           // If BUZZ is greater than 350
     {
-      lcd.string("BUZZER");
-      PORTD = 0b01000000;
+      lcd.string("Alert! ");                  // Print on the LCD Alert!
+      PORTD = 0b01000000;                     // Set PD6 as High and everything else Low
     }
     
-    if((LED < 350) && (BUZZ < 350))
+    if((LED > 350) && (BUZZ > 350))           // If LED & BUZZ are greater than 350 
     {
-      PORTD = 0b00000000;
+      PORTD = 0b00000000;                     // Turn Everything off
     }
-    if(PIND & 0x04 )
+    if(PIND & 0x04 )                          // If Button 1 is Pressed 
     {
-      lcd.cmd(0x01);
-      
-    }
+      lcd.cmd(0x01);                          // Clear LCD screen
     }
   }
+}
