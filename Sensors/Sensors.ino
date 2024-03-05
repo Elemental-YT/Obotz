64bc26b175b8b2f08142da632ba34db02da8654c
Sodavaram <sodavaram@sodavarams-mbp.home>
2024-03-05 18:30:21 -0500

    Not fully doen but made some changes to sensor code

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,43 +1,39 @@
+// This Section is to Create Global Variables, Define Objects, and Include library's
 #include<avr/io.h>                                  // Include library avr/io.h 
 #include<util/delay.h>                              // Include delay library
 #define checkbit(x,y) ((x) &(y))                    // Create Macro checkbit
 #define bitn(p) (0x01 << (p))                       // Create Macro bitn
-
 int main(void)                                      // Begin code
 {
+  // This Section is to Create Local Variables, Start serial moniter and set Serial Moniter's Baud Rate to 2000000, and finally set the Input Output Ports
   Serial.begin (2000000);                           // Start serial moniter and set BAUD RATE to 2000000
-  int xval, yval, zval, val;                        // Create Variables Xval for X-axis( Left & Right ) , Yval for  Y-axis (Up & Down) , Zval for Z-axis ( Depth ), And Val to store the IR Sensor's Value.
-    
-    while(1)                                        // Begin Infinite Loop
-    {
-      // This Whole Section is to fetch the Accelerometer's Value 
-       ADMUX = 0x45;                                // Set the Analog to Digatal Converter to Port A (Analog) Pin 5  
-       ADCSRA = 0xC7;                               // Begin the Analog to Digatal Conversion to Port A Pin 5 
-       while (checkbit(ADCSRA, bitn(ADSC)));        // Wait until Analog to Digatal Converter is complete 
-       xval =ADCW;                                  // Save Port A Pin 5's Digatal Value on to Variable xval               
-       Serial.print("X-axis: ");                    // Print "X-axis: " on to the Serial Moniter 
-       Serial.print(xval);                          // Print Variable xval's Value on to the Serial Moniter  
-       
-       ADMUX = 0x46;                                // Set the Analog to Digatal Converter to Port A (Analog) Pin 6  
-       ADCSRA = 0xC7;                               // Begin the Analog to Digatal Conversion         
-       while(checkbit(ADCSRA, bitn(ADSC)));         // Wait until the Analog to Digatal Converter is complete
-       yval = ADCW;                                 // Save Port A Pin 6's Digital Value on to Variable yval
-       Serial.print(" Y-axis ");                    // Print " Y-axis " on to the Serial Moniter 
-       Serial.print(yval);                          // Print Variable yval's Value on to the Serial Moniter 
-       
-       ADMUX = 0x47;                                // Set theAnalog to Digatal Converter to Port A Pin 7
-       ADCSRA = 0xC7;                               // Begin the Analog to Digatal Converter     
-       while(checkbit(ADCSRA, bitn(ADSC)));         // Wait until the Analog to Digatal Convertesion is Complete
-       zval = ADCW;                                 // Save Port A Pin 7's Digital Value on to Variable zval
-       Serial.print(" Z-axis ");                    // Print " Z-axis " on to the Serial Moniter 
-       Serial.println(zval);                        // Print Variable zval's Value on to the Serial Moniter then create a new line                             
-       // This next section is to fetch the IR sensor's Value
+  int Data;                                         // Create Variable Val to store the IR Sensor's Value.
+  int Recab = 100;                                  // Create Variable Recab to store Recalibration data
+  DDR? = 0x?? 
+  while(1)                                        // Begin Infinite Loop
+    {                            
+       // This section is to fetch the IR sensor's Value
        ADMUX = 0x40;                                // Set the Analog to Digital Converter to Port A Pin 0
        ADCSRA = 0xC7;                               // Begin the Analog to Digital Conversion
        while (checkbit(ADCSRA, bitn(ADSC)));        // Wait until the Analog to Digial Conversion is Complete
-       val = ADCW;                                  // Set Port A Pin 0's Digital Value to Variable VAL
+       Data = ADCW;                                  // Set Port A Pin 0's Digital Value to Variable VAL
        Serial.print("Sensor's Val: ");               // Print "Sensor's Val: " on to the Serial Moniter
-       Serial.println(val);                          // Print Variable val's Value on to the Serial Moniter
-       _delay_ms(600);                               // Wait 600 Milliseconds
+       Serial.println(Data);                          // Print Variable val's Value on to the Serial Moniter
+       if(checkbit(PORT?, bitn(?)))                   // If Switch 1 is pressed
+        {
+          Recab = Data;                               // Set Recab's Value to Data's Value
+        }
+
+        if(Data = Recab)                              // If Data equals Recab           
+        {
+          setbit(PORT?, bitn(?));                     // Start Moter ALERT!
+          clearbit(PORT?, bitn(?));                   // Retract Moter Quiet
+        }
+
+        else
+        {
+          setbit(PORT?, bitn(?));                    // Start Moter Quiet
+          clearbit(PORT?, bitn(?));                  // Stop Moter ALERT!
+        }
     } 
 }
